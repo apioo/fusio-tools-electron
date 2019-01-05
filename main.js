917a9ba06e147326714ed5ee57ff7660dffb07dc
Christoph Kappestein <christoph.kappestein@gmail.com>
2019-01-05 22:14:47 +0100

    ask for endpoint url

--- main.js
+++ main.js
@@ -2,57 +2,87 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 const fs = require('fs');
+const config = require('electron-json-config');
+const prompt = require('electron-prompt');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 1024, height: 768})
-  win.maximize();
-  
-  // get config
-  var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({width: 1024, height: 768})
+    win.maximize();
 
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.htm'),
-    protocol: 'file:',
-    query: config,
-    slashes: true
-  }))
+    let endpoint = config.get('endpoint');
+    if (!endpoint) {
+        // ask for endpoint url
+        prompt({
+            title: 'Fusio endpoint',
+            label: 'Url:',
+            value: 'http://demo.fusio-project.org/',
+            inputAttrs: {
+                type: 'url'
+            }
+        }).then((endpoint) => {
+            if (endpoint === null) {
+                // no endpoint defined
+            } else {
+                config.set('endpoint', endpoint);
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+                // and load the index.html of the app.
+                win.loadURL(url.format({
+                    pathname: path.join(__dirname, 'index.htm'),
+                    protocol: 'file:',
+                    slashes: true,
+                    query: {
+                        url: endpoint
+                    }
+                }))
+            }
+        });
+    } else {
+        // and load the index.html of the app.
+        win.loadURL(url.format({
+            pathname: path.join(__dirname, 'index.htm'),
+            protocol: 'file:',
+            slashes: true,
+            query: {
+                url: endpoint
+            }
+        }))
+    }
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
+});
 
 
 
