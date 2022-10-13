f38ed0c4884dcc1fe62f1bf163d7a0eea97069db
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-10-13 16:17:22 +0200

    update main

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
-const fs = require('fs');
-const config = require('electron-json-config');
+const config = require('electron-json-config').factory();
 const prompt = require('electron-prompt');
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -10,9 +9,10 @@ const prompt = require('electron-prompt');
 let win
 
 function createWindow() {
-    // Create the browser window.
-    win = new BrowserWindow({width: 1024, height: 768})
-    win.maximize();
+    const win = new BrowserWindow({
+        width: 1024,
+        height: 768,
+    })
 
     let endpoint = config.get('endpoint');
     if (!endpoint) {
@@ -32,7 +32,7 @@ function createWindow() {
 
                 // and load the index.html of the app.
                 win.loadURL(url.format({
-                    pathname: path.join(__dirname, 'index.htm'),
+                    pathname: path.join(__dirname, '/dist/index.html'),
                     protocol: 'file:',
                     slashes: true,
                     query: {
@@ -44,7 +44,7 @@ function createWindow() {
     } else {
         // and load the index.html of the app.
         win.loadURL(url.format({
-            pathname: path.join(__dirname, 'index.htm'),
+            pathname: path.join(__dirname, '/dist/index.html'),
             protocol: 'file:',
             slashes: true,
             query: {
@@ -52,38 +52,20 @@ function createWindow() {
             }
         }))
     }
-
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null
-    })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+    createWindow()
 
-// Quit when all windows are closed.
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit()
     }
-});
-
-app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow()
-    }
-});
-
-
-
-
+})
